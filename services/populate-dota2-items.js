be9ead3fadbaa9dde9e11db9a4e76f82cd2637ad
Rory Duncan <robust.rory@gmail.com>
2018-08-13 03:01:29 +0000

    Add images to item data process

--- services/populate-dota2-items.js
+++ services/populate-dota2-items.js
@@ -1,6 +1,18 @@
 /*
  fetches the dota 2 list of in-game items from the steam API, then adds them to our firebase RTDB
+ 
+ todo: fetch image url's too
 */
+
+
+// mutator
+const getItemImage = (item) => {
+	let name = item.name.replace(/item_/gi, '') + '_';
+	item.image = 'http://cdn.dota2.com/apps/dota2/images/items/' + name + 'lg.png';
+	return item;
+}
+
+const getItemImages = (items) => items.map(getItemImage);
 
 require('dotenv-safe').config()
 const firebaseApp = require("./firebase.js");
@@ -15,9 +27,10 @@ if (steamAPIKey) getItems();
 else throw new Error("Missing Steam API Key")
 
 async function getItems() {
-  const result = await fetch(`https://api.steampowered.com/IEconDOTA2_570/getGameItems/v0001/?key=${steamAPIKey}`)
+  await fetch(`https://api.steampowered.com/IEconDOTA2_570/getGameItems/v0001/?key=${steamAPIKey}`)
     .then(response => response.json())
     .then(json => json.result.items)
+    .then(items => getItemImages(items))
     .then(items => db.ref("dota2/items").set(items))
     .then(() => console.log("Items added."))
     .catch(err => console.log(err));
