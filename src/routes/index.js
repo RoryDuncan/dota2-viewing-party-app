d0d5fc361c5e314cb9766db89cb7a89bc85576ef
Rory Duncan <robust.rory@gmail.com>
2018-08-13 05:43:11 +0000

    Add AppLoader and first app

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,18 @@ import Router from "helpers/router";
 
 // pages
 import Index from "pages/Index.html";
+import AppLoader from "components/AppLoader.html";
 
 
 const router = new Router("index");
 
 // landing route
 router.route("/", context => router.view.set({ Page: Index }));
+
+router.route("/app/:appname", context => router.view.set({ 
+  Page: AppLoader, 
+  pageData: context.params.appname
+}));
 
 // router.route("/loading", context => router.view.set({ Page: Loading }));
 
